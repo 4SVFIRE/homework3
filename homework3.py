a63831c2d23ad24087b2b975089db30f61f22381
FIRE <133657078+4SVFIRE@users.noreply.github.com>
2024-07-24 11:17:09 +0300

    Add files via upload

--- homework3.py
+++ homework3.py
@@ -2,7 +2,7 @@ name = 'Artem'
 print('Name:',name)
 age = 20
 print('Age:',age)
-new_age = age + 1
-print('New Age:',new_age)
+age += 1
+print('New Age:',age)
 is_student = True
 print('Is student:',is_student)
